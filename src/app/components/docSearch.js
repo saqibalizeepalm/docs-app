c465a21667e57307780f3ac36921f0daf6d06dcb
iamsaqibwahla@gmail.com <iamsaqibwahla@gmail.com>
2024-10-31 17:32:55 +0500

    add changes

--- src/app/components/docSearch.js
+++ src/app/components/docSearch.js
@@ -6,28 +6,6 @@ import 'docsearch.js/dist/cdn/docsearch.css';
 import '@docsearch/css/dist/style.css'; 
 
 const DocuSearch = () => {
-//   useEffect(() => {
-//     const timer = setTimeout(() => {
-//       const inputExists = document.getElementById('docsearch');
-//       console.log('-------', inputExists);
-//       if (inputExists) {
-//         docsearch({
-//           apiKey: '4c7704154788f0bbf566b15b21c35c5e',
-//           appId: 'T11OTANJ7U',
-//           indexName: 'app-pearl',
-//           container: '#docsearch',
-//           placeholder: 'search...',
-//           debug: false,
-//         });
-//       } else {
-//         console.error('Input field does not exist!');
-//       }
-//     }, 0);
-
-//     return () => {
-//       clearTimeout(timer);
-//     };
-//   }, []);
 
   return (
     <div className='flex justify-center'>
@@ -36,7 +14,6 @@ const DocuSearch = () => {
         apiKey="158ae7fb7ab370014a1df386cf0b9f2d" // Replace with your Algolia search-only API key
         indexName="app-pearl" 
         placeholder="..."
-        // className="absolute top-4 left-4 w-100" // Add your desired styling
       />
       </div>
   );
