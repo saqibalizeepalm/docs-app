1343762ed931ec500778d40c20c2cfb0e9ed1269
iamsaqibwahla@gmail.com <iamsaqibwahla@gmail.com>
2024-11-04 16:36:20 +0500

    app id added

--- src/app/components/docSearch.js
+++ src/app/components/docSearch.js
@@ -10,8 +10,8 @@ const DocuSearch = () => {
   return (
     <div className='flex justify-center'>
        <DocSearch
-        appId="T11OTANJ7U" // Replace with your Algolia app ID
-        apiKey="158ae7fb7ab370014a1df386cf0b9f2d" // Replace with your Algolia search-only API key
+        appId="8MS2S5MXZR" // Replace with your Algolia app ID
+        apiKey="92e4a4992547d97ce812e4ff8e7f6cb3" // Replace with your Algolia search-only API key
         indexName="app-pearl" 
         placeholder="..."
       />
